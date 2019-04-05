349bcb07a2dd84b9e3648a54cce812293cd797cd
Yao <46439836+sukoyao@users.noreply.github.com>
2019-04-05 17:11:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || 
+      restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || 
+      restaurant.category.includes(keyword)
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
